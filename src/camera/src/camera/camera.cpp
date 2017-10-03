fb5753692b495139088e89cefcc59b6734de071a
mohammad <mohammadjv94@gmail.com>
2017-10-03 14:59:24 +0330

    rgb image capturing from camera added

--- src/camera/src/camera/camera.cpp
+++ src/camera/src/camera/camera.cpp
@@ -5,22 +5,26 @@
 #include <iostream>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <vector>
-
+#include <messages/Image.h>
 
 using namespace std;
 using namespace cv;
 using namespace image_transport;
 
+
 int main(int argc , char **argv){
     ros::init(argc,argv,"camera_node");
     ros::NodeHandle nh;
     ImageTransport it(nh);
+    cv::VideoCapture cap(0);
     Publisher publisher = it.advertise("/camera/image",1);
     ros::Rate loopRate(30);
+    cv::Mat rgb;
     while(nh.ok()){
-	std::cout << "khar!\n";
-        ros::spinOnce();
-        loopRate.sleep();
+	     cap >> rgb;
+       cv::imshow("rgb image",rgb);
+       cv::waitKey(10);
+       ros::spinOnce();
+       loopRate.sleep();
     }
 }
-
