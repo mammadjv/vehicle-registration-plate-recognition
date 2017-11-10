1b37df0f5d608259502f8c61e4d6c9485d7eb73c
mohammad <mohammadjv94@gmail.com>
2017-11-10 16:40:41 +0330

    self.cvbridge removed

--- src/plate_detector/scripts/plateDetectorBase.py
+++ src/plate_detector/scripts/plateDetectorBase.py
@@ -2,6 +2,7 @@
 import rospy
 from std_msgs.msg import String
 from system_messages.msg import Image
+from system_messages.msg import Plates
 from plateDetector import PlateDetector
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
@@ -9,12 +10,13 @@ import cv2
 class PlateDetectorBase(PlateDetector):
 	def __init__(self):
 		PlateDetector.__init__(self)
-		self.bridge = CvBridge()
 		rospy.init_node('plate_detector_node', anonymous=True)
-		rospy.Subscriber("/camera/image", Image, self.on_image_received)
+		image_subscriber = rospy.Subscriber("/image", Image, self.on_image_received)
+		plates_publisher = rospy.Publisher('/plates', Plates, queue_size=10)
+
 	def on_image_received(self, image):
-		rgb_image = self.bridge.imgmsg_to_cv2(image.rgb, "bgr8")
-		scharred_image = self.bridge.imgmsg_to_cv2(image.scharred, "mono8")
+		rgb_image = CvBridge().imgmsg_to_cv2(image.rgb, "bgr8")
+		scharred_image = CvBridge().imgmsg_to_cv2(image.scharred, "mono8")
 		cv2.imshow('rgb',rgb_image)
 		cv2.waitKey(1)
 		print "image received!"
