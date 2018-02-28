a20a131a63fc16ee5f1dafe9c74288537c92b176
mohammad <mohammadjv94@gmail.com>
2018-02-28 16:53:41 +0330

    the source code for random plate generation modified

--- generate_random_plates.py
+++ generate_random_plates.py
@@ -8,6 +8,39 @@ from random import randint
 from random import shuffle
 import random
 import struct
+import imutils
+import colorsys
+
+#def rgb2hsv (r,g,b):
+#	computedH = 0
+#	computedS = 0
+#	computedV = 0
+ #	r=r/255
+#	g=g/255
+#	b=b/255
+ #	minRGB = Math.min(r,Math.min(g,b))
+ #	maxRGB = Math.max(r,Math.max(g,b))
+ 	# Black-gray-white
+#	if (minRGB==maxRGB):
+#		computedV = minRGB;
+ # 		return [0,0,computedV];
+
+	# Colors other than black-gray-white:
+#	d = (r==minRGB) ? g-b : ((b==minRGB) ? r-g : b-r)
+#	h = (r==minRGB) ? 3 : ((b==minRGB) ? 1 : 5)
+#	computedH = 60*(h - d/(maxRGB - minRGB))
+#	computedS = (maxRGB - minRGB)/maxRGB
+#	computedV = maxRGB
+#	return [computedH,computedS,computedV]
+
+	
+
+
+def rotateImage(image, angle):
+  image_center = tuple(np.array(image.shape[1::-1]) / 2)
+  rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+  result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+  return result
 
 
 def hex2rgb(rgb):
@@ -23,6 +56,7 @@ def get_annotation(annots,image_name):
 			return annot
 	return None
 
+#print rgb2hex([60,55,61])
 def get_contours_bounding_rect(contours):
 	bounding_rects = []
 	for cnt in contours:
@@ -50,23 +84,13 @@ def get_contour(image, cnt):
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 	kernel = np.ones((3,3),np.uint8)
 	
-#	gray = cv2.erode(gray,kernel,iterations = 1)
-#	gray = cv2.dilate(gray,kernel,iterations = 1)
-#	gray = cv2.dilate(gray,kernel,iterations = 1)
-#	gray = cv2.erode(gray,kernel,iterations = 1)
-#	gray = cv2.erode(gray,kernel,iterations = 1)
-#	gray = cv2.dilate(gray,kernel,iterations = 1)
-#	gray = cv2.erode(gray,kernel,iterations = 1)
+	ret,thresh = cv2.threshold(gray,120,255,cv2.THRESH_BINARY_INV)
+	
+	whites =  float(len(np.where((thresh == 255))[0]))
+	blacks =  float(len(np.where((thresh == 0))[0]))
 
-#	ret,thresh = cv2.threshold(gray,80,255,cv2.THRESH_BINARY_INV)
-	#thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
-	#thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
-	#thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-#	cv2.imshow('gray_open_closed' , image)
-	thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,15,2)
-
-#	thresh = cv2.erode(thresh,kernel,iterations = 1)
-#	thresh = cv2.dilate(thresh,kernel,iterations = 1)
+	if ( (whites)/(whites+blacks) < 0.4 or (blacks)/(whites+blacks) < 0.4):
+		return None
 
 	__, contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -79,16 +103,12 @@ def get_contour(image, cnt):
 
 def get_random_image(image, desired_list, cnt):
 	while(1==1):
-		num_index = random.sample(xrange(0,len(desired_list)+1), 1)[0]
+		num_index = random.sample(xrange(0,len(desired_list)-1), 1)[0]
 		number_image = cv2.imread(desired_list[num_index])
 		number_image = cv2.resize(number_image,(cnt['x_end']-cnt['x_begin'],cnt['y_end']-cnt['y_begin']))
 		generated_images = get_contour(number_image,cnt)
-#		print generated_images
 		if(generated_images != None):
 			return generated_images
-#		cv2.imshow('image',colored_image_croped)
-#		cv2.imshow('thresh',thresh_image_croped)
-#		cv2.waitKey(0)
 
 
 
@@ -154,44 +174,64 @@ for char_folder in char_folders:
 
 shuffle(chars)
 
+number_of_generated_plates = 0
 for plate_pic in pic_files:
 	plate = cv2.imread(pic_path+plate_pic)
 	annot = get_annotation(annots,plate_pic)
-	print plate_pic
 	image = plate
-	i = 0
+#	i = 0
 	black_code = ""
 	white_code = ""
-	for cnt in annot:
-		if(i == 0):
-			image = plate
-			black_code = hex2rgb(cnt['black_code'])[::-1]
-			print str(cnt['black_code']) + '   black_code'
-			white_code = hex2rgb(cnt['white_code'])[::-1]
-			print str(cnt['white_code']) + '   white_code'
-			image = cv2.resize(image,(370,83))
-		else:
-			x_begin = cnt['x_begin']
-			y_begin = cnt['y_begin']
-			x_end = cnt['x_end']
-			y_end = cnt['y_end']
-#			cv2.rectangle(image,(x_begin,y_begin),(x_end,y_end),(0,(y_end*y_begin)%255,(x_begin*x_end)%255),1)
-			sample_thresh = plate
-			sample_colored = plate
-			if(i != 3):
-				[sample_thresh, sample_colored] = get_random_image(image, numbers, cnt)
+	number_of_generated_plates = number_of_generated_plates + 1
+	while(number_of_generated_plates % 500 != 0):
+		i = 0
+		for cnt in annot:
+			if(i == 0):
+				image = plate
+				black_code = hex2rgb(cnt['black_code'])[::-1]
+				[h ,s , v] = colorsys.rgb_to_hsv(black_code[0],black_code[1],black_code[2])
+				black_code = colorsys.hsv_to_rgb(h,s,abs(v+random.sample(xrange(-60,60), 1)[0]))
+				white_code = hex2rgb(cnt['white_code'])[::-1]
+				image = cv2.resize(image,(370,83))
 			else:
-				[sample_thresh, sample_colored] = get_random_image(image, chars, cnt)
-			white_positions =  np.where((sample_thresh == 255))
-			black_positions =  np.where((sample_thresh == 0))
-			sample_colored[white_positions] = black_code
-			sample_colored[black_positions] = white_code
-			sample_colored = cv2.resize(sample_colored,(x_end - x_begin,y_end-y_begin))
-			image[cnt['y_begin']:cnt['y_end'],cnt['x_begin']:cnt['x_end']] = sample_colored
-#			cv2.imshow('thresh',sample_thresh)
-#			cv2.imshow('colored',sample_colored)
-#			cv2.waitKey(2)
-		i = i + 1
+				x_begin = cnt['x_begin']
+				y_begin = cnt['y_begin']
+				x_end = cnt['x_end']
+				y_end = cnt['y_end']
+				sample_thresh = plate
+				sample_colored = plate
+				if(i != 3):
+					[sample_thresh, sample_colored] = get_random_image(image, numbers, cnt)
+				else:
+					[sample_thresh, sample_colored] = get_random_image(image, chars, cnt)
+				white_positions =  np.where((sample_thresh == 255))
+				black_positions =  np.where((sample_thresh == 0))
+				sample_colored[white_positions] = black_code
+				sample_colored[black_positions] = white_code
+	#			cv2.imshow('colored before',sample_colored)
+	#			sample_colored = adjust_gamma(sample_colored, white_positions, gamma=0.5)
+				sample_colored = cv2.resize(sample_colored,(x_end - x_begin,y_end-y_begin))
+				image[cnt['y_begin']:cnt['y_end'],cnt['x_begin']:cnt['x_end']] = sample_colored
+	#			cv2.imshow('brighter',new)
+	#			cv2.imshow('thresh',sample_thresh)
+	#			cv2.imshow('colored',sample_colored)
+	#			cv2.waitKey(0)
+			i = i + 1
+		kernel = np.ones((3,3),np.uint8)
+	#	image = cv2.erode(image,kernel,iterations = 1)
+		gamma = float(random.sample(xrange(8,13), 1)[0])/10.00
+		image = cv2.dilate(image,kernel,iterations = 1)
+		image = adjust_gamma(image,gamma)
+		image = cv2.resize(image,(180,42))
+		rotated = imutils.rotate_bound(image, random.sample(xrange(-2,4), 1)[0])
+	#	rotated = rotateImage(image, 2)
+		cv2.imshow("Rotated (Correct)", rotated)
+#		cv2.imshow('rgb', image)
+		cv2.imwrite('./generated_plates/'+str(number_of_generated_plates)+'.jpg',rotated)
+		cv2.waitKey(2)
+		number_of_generated_plates = number_of_generated_plates + 1
+
+
 #	for gamma in np.arange(0.5, 1.7, 0.5):
 		# ignore when gamma is 1 (there will be no change to the image)
 #		if gamma == 1:
@@ -204,10 +244,3 @@ for plate_pic in pic_files:
 #		cv2.imshow("Images", np.hstack([image, adjusted]))
 #		cv2.imwrite(pic_path+image_name, adjusted)
 #		cv2.waitKey(0)
-#	print random.sample(xrange(1,50), 48)
-
-	cv2.imshow('rgb', image)
-	cv2.waitKey(0)
-sys.exit()
-image = None
-
