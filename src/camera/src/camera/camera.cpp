bf57e62ea1eba06344af024831b7e02fa76e5cf4
mammadjv <mohammadjv94@gmail.com>
2018-03-01 23:35:57 +0330

    connection between nodes and system life cycle is changed

--- src/camera/src/camera/camera.cpp
+++ src/camera/src/camera/camera.cpp
@@ -6,34 +6,49 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <vector>
 #include <system_messages/Image.h>
+#include <std_msgs/Bool.h>
+
+cv::VideoCapture cap("/home/mohammad/catkin_ws/PersonRecognition/python_scripts/dataset/rgb_2.avi");
+cv::Mat rgb, gray, scharred_image;
+
+class ImagePubSub{
+public:
+	ros::Publisher publisher;
+	ros::Subscriber sub;
+	void cycleCallback(const std_msgs::Bool::ConstPtr& cycle_completed){
+                if(cycle_completed->data == true){
+                        if (!cap.read(rgb))
+                                return;
+                        this->publish(rgb);
+                }
+        };
+
+	ImagePubSub(ros::NodeHandle nh){
+	    	publisher = nh.advertise <system_messages::Image> ("/image",1);
+    		sub = nh.subscribe("/cycle_completed", 1000, &ImagePubSub::cycleCallback,this);
+	};
+
+	void publish(cv::Mat rgb){
+	        system_messages::Image::Ptr image = boost::make_shared<system_messages::Image>();
+	        cv::cvtColor(rgb,gray,CV_BGR2GRAY);
+	        cv::blur(gray, gray,cv::Size(5,5),cv::Point(-1,-1),cv::BORDER_DEFAULT);
+        	cv::Scharr(gray,scharred_image,CV_8U,0,1,1,0,cv::BORDER_DEFAULT);
+	        sensor_msgs::ImagePtr rgb_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", rgb).toImageMsg();
+        	sensor_msgs::ImagePtr scharred_msg = cv_bridge::CvImage(std_msgs::Header(),"mono8",scharred_image).toImageMsg();
+	        image->rgb = *rgb_msg;
+        	image->scharred = *scharred_msg;
+        	this->publisher.publish(image);
+	};
+};
 
 int main(int argc , char **argv){
     ros::init(argc,argv,"camera_node");
     ros::NodeHandle nh;
-    ros::Publisher publisher = nh.advertise <system_messages::Image> ("/image",1);
-    ros::Rate loopRate(30);
-
-    cv::VideoCapture cap("/home/mohammad/catkin_ws/PersonRecognition/python_scripts/dataset/rgb_2.avi");
-    cv::Mat rgb, gray, scharred_image;
-
-    system_messages::Image::Ptr image = boost::make_shared<system_messages::Image>();
-
-    while(nh.ok()){
-        // capture and convert the image
-	if (!cap.read(rgb))
-            break;
-
-        cv::cvtColor(rgb,gray,CV_BGR2GRAY);
-        cv::blur(gray, gray,cv::Size(5,5),cv::Point(-1,-1),cv::BORDER_DEFAULT);
-        cv::Scharr(gray,scharred_image,CV_8U,0,1,1,0,cv::BORDER_DEFAULT);
-
-        sensor_msgs::ImagePtr rgb_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", rgb).toImageMsg();
-        sensor_msgs::ImagePtr scharred_msg = cv_bridge::CvImage(std_msgs::Header(),"mono8",scharred_image).toImageMsg();
-        image->rgb = *rgb_msg;
-        image->scharred = *scharred_msg;
-        publisher.publish(image);
-        ros::spinOnce();
-        loopRate.sleep();
-    }
+    ImagePubSub *image_pub_sub = new ImagePubSub(nh);
+    if (!cap.read(rgb))
+    	return 0;
+    image_pub_sub->publish(rgb);
+//    ros::spinOnce();
+    ros::spin();
 return 0;
 }
--- src/plate_detector/scripts/plateDetector.py
+++ src/plate_detector/scripts/plateDetector.py
@@ -1,8 +1,15 @@
 import numpy
 import cv2
+import sys
+
+sys.path.insert(0,"/home/mj/workspace/py-faster-rcnn/tools")
+
+from get_plates_location import PositionDetector 
 
 class PlateDetector:
 	def __init__(self):
 		print "plate_detector module created!"
+		self.position_detector = PositionDetector()
 	def find_location_of_plate(self, image):
 		print 'toooodooooo'
+		bboxes = self.position_detector.detect(image)
