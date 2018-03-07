ac79eef98c3bfa84d32d87be8fe7744a07542753
mammadjv <mohammadjv94@gmail.com>
2018-03-07 10:30:47 +0330

    the bug of drawing image publishing fixed

--- src/plate_detector/scripts/plateDetectorBase.py
+++ src/plate_detector/scripts/plateDetectorBase.py
@@ -18,7 +18,9 @@ class PlateDetectorBase(PlateDetector):
 		self.image_subscriber = rospy.Subscriber("/image", ImageMsg, self.on_image_received)
 		self.plates_publisher = rospy.Publisher('/plates', Plates, queue_size=1000)
 		self.cycle_complete_publisher = rospy.Publisher('/cycle_completed',Bool,queue_size = 1)
+		self.show_image_publisher = rospy.Publisher('/show_image', Image, queue_size=1)
 		self.r = rospy.Rate(10)
+		self.bridge = CvBridge()
 	def on_image_received(self, image):
 		rgb_image = CvBridge().imgmsg_to_cv2(image.rgb, "bgr8")
 		scharred_image = CvBridge().imgmsg_to_cv2(image.scharred, "mono8")
@@ -32,10 +34,12 @@ class PlateDetectorBase(PlateDetector):
  #               self.cycle_complete_publisher.publish(cycle_completed_msg)
 #		return
 #		print rgb_image.shape
-#		bboxes = self.find_location_of_plate(rgb_image)
-		bboxes = list()
-		bboxes.append([1 , 2 , 3, 4])
+		bboxes = self.find_location_of_plate(rgb_image)
+#		bboxes = list()
+#		bboxes.append([1 , 2 , 3, 4])
 #		bboxes = None
+#		paint_msg = None
+		paint_image = rgb_image.copy()
 		if(len(bboxes) == 0):
 #			print "no boudning box extracted"
 			cycle_completed_msg = Bool()
@@ -52,7 +56,10 @@ class PlateDetectorBase(PlateDetector):
 				plate_msg.down_right.x = bbox[2]
 				plate_msg.down_right.y = bbox[3]
 				plates_msg.plates.append(plate_msg)
+				cv2.rectangle(paint_image,(bbox[0],bbox[1]),(bbox[2],bbox[3]),(0,255,0),3)
 			self.plates_publisher.publish(plates_msg)
+		self.show_image_publisher.publish(self.bridge.cv2_to_imgmsg(paint_image, "bgr8"))
+		
 #		self.r.sleep()
 
 if __name__ == '__main__':
